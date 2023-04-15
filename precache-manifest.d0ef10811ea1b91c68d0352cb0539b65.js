b950e49bf4658dbc22aff8c13bc3c6eb353844b2
Magdi <magditiea@gmail.com>
2023-04-15 14:01:23 -0400

    Updates

---
+++ precache-manifest.d0ef10811ea1b91c68d0352cb0539b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629f6d5ec75617572f2b9de3f94000c2",
+    "url": "/chatApp/index.html"
+  },
+  {
+    "revision": "27748812ff6d023395b7",
+    "url": "/chatApp/static/css/main.c62e1144.chunk.css"
+  },
+  {
+    "revision": "d6484c5ae7c5e6a55004",
+    "url": "/chatApp/static/js/2.513ca247.chunk.js"
+  },
+  {
+    "revision": "27748812ff6d023395b7",
+    "url": "/chatApp/static/js/main.07afb6cc.chunk.js"
+  },
+  {
+    "revision": "521623c52e341d308709",
+    "url": "/chatApp/static/js/runtime~main.81acb6c9.js"
+  }
+]);
